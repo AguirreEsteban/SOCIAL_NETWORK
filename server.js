36f20495c0811eabeccc89ad2d21ec8e0dafd80b
AguirreEsteban <diazaguirre1995@gmail.com>
2022-06-20 19:10:37 -0500

    Update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { ApolloServer, AuthenticationError } = require("apollo-server");
+﻿const { ApolloServer, AuthenticationError } = require("apollo-server");
 const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
@@ -19,6 +19,8 @@ mongoose
   .connect(
     process.env.MONGO_URI,
     { useNewUrlParser: true }
+
+
   )
   .then(() => {
     console.log("BD conectada");
